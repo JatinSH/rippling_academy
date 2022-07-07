19a99be54adf2d40063d1a1438aa6dc2307e553c
Jatin Goel <44617146+JatinSH@users.noreply.github.com>
2022-07-08 00:46:38 +0530

    fix: modularized the tic tac toe code

--- week-4/tic-tac-toe/main.py
+++ week-4/tic-tac-toe/main.py
@@ -1,47 +1,35 @@
-class game_board:
-
-    def __init__(self, tic_tac_toe_grid):
-        self.tic_tac_toe_grid = tic_tac_toe_grid
-        self.grid_rows = len(tic_tac_toe_grid)
-        self.grid_columns = len(tic_tac_toe_grid[0])
-
-    def display_grid(self):
-
-        for grid_row in self.tic_tac_toe_grid:
-            print("\n  ---   ---   ---\n|", end="")
-            for item in grid_row:
-                print("  %s  |" % (item), end="")
-        print("\n  ---   ---   ---")
-
-    def is_position_valid(self, x_positon, y_position):
-        if(self.tic_tac_toe_grid[x_positon][y_position] == " "):
-            return True
-        else:
-            return False
-
-    def update_grid(self, x_position, y_position, player_symbol):
-        if(self.is_position_valid(x_position, y_position)):
-            self.tic_tac_toe_grid[x_position][y_position] = player_symbol
-            return True
-        else:
-            return False
-
+import os
+from game_board import game_board
+from game_start import game_start
 
 initial_board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 game = game_board(initial_board)
+[player1, player2] = game_start()
+current_player = player1
 
+while(True):
 
-class player:
-    pass
+    game.display_grid()
+    print("\n", current_player.player_name,
+          ", enter coordinates in the form of \"row,column\" (1 index based) :  ", end="")
 
+    user_input = input()
+    x_position = int(user_input[0])
+    y_position = int(user_input[2])
 
-print("WELCOME TO THE SQUID GAME'S : TIC TAC TOE!!")
-game.display_grid()
-print("Enter Coordinates: ")
-user_input = input()
-x_position = int(user_input[0])
-print(x_position)
-y_position = int(user_input[2])
-print(y_position)
-game.update_grid(x_position, y_position, "O")
-game.display_grid()
+    if(not game.update_grid(x_position, y_position, current_player.player_symbol)):
+        os.system('clear')
+        print("The position is already filled!!")
+        continue
+
+    if(game.has_game_ended()):
+        os.system('clear')
+        game.display_grid()
+        print("\n", current_player.player_name, " WINS!!")
+        break
+
+    if(current_player == player1):
+        current_player = player2
+    else:
+        current_player = player1
+    os.system('clear')
